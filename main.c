0f160af598d8a4579dbd469bb6e58dc2de5aca8c
Sh1roKaze <Sh1roKaze@users.noreply.github.com>
2016-04-10 22:47:11 +0200

    Update main.c

--- main.c
+++ main.c
@@ -3,12 +3,12 @@
     
 static void activate (GtkApplication* app, gpointer user_data) {
     
-  GtkWidget *window;
+  GtkWidget *window_main;
 
-  window = gtk_application_window_new (app);
-  gtk_window_set_title (GTK_WINDOW (window), "Window");
-  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
-  gtk_widget_show_all (window);
+  window_main = gtk_application_window_new (app);
+  gtk_window_set_title (GTK_WINDOW (window_main), "Calculator");
+  gtk_window_set_default_size (GTK_WINDOW (window_main), 200, 200);
+  gtk_widget_show_all (window_main);
   
 }
 
