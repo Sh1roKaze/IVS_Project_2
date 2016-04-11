e2d82781bfa88bcd8e28868409e083e8392e2042
ShiroKaze <shiro@localhost.localdomain>
2016-04-12 00:59:06 +0200

    comments

--- main.c
+++ main.c
@@ -24,23 +24,31 @@ int main (int argc, char **argv) {
   App *calculator;
   GtkBuilder *calcBuilder;
   
+  //allocates mem for an App
   calculator = g_slice_new(App);
 
+  //inits gtk
   gtk_init (&argc, &argv);
   
+  //inits interface builder
   calcBuilder = gtk_builder_new_from_file("interface.glade");
   
+  //link the window_main in builder to calculator->window
   calculator->window = GTK_WIDGET(gtk_builder_get_object (calcBuilder, "window_main"));
   
+  //connects signals
   gtk_builder_connect_signals (calcBuilder, calculator);
 
+  //frees calcBuilder
   g_object_unref (G_OBJECT (calcBuilder));
-    
+  
+  //connects signal for destruction with window  
   g_signal_connect (calculator->window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
         
+  //bring window into spotlight
   gtk_widget_show (calculator->window);                
 
-	//hlavni smycka gtk
+  //main gtk loop
   gtk_main ();
 
 
