8c1cd61827d7050e4a4b5e6968c7e6d81243cbaf
ShiroKaze <shiro@localhost.localdomain>
2016-04-13 00:54:21 +0200

    Get all the buttons

--- main.c
+++ main.c
@@ -52,10 +52,29 @@ int main (int argc, char **argv) {
   //inits interface builder
   calcBuilder = gtk_builder_new_from_file("interface.glade");
   
-  //link the window_main in builder to calculator->window
+  //link the objects created by builder to the "calculator" structure
   calculator->window_main = GTK_WIDGET(gtk_builder_get_object (calcBuilder, "window_main"));
-  
   calculator->button0 = GTK_WIDGET(gtk_builder_get_object (calcBuilder, "button0"));
+  calculator->button1 = GTK_WIDGET(gtk_builder_get_object (calcBuilder, "button1"));
+  calculator->button2 = GTK_WIDGET(gtk_builder_get_object (calcBuilder, "button2"));
+  calculator->button3 = GTK_WIDGET(gtk_builder_get_object (calcBuilder, "button3"));
+  calculator->button4 = GTK_WIDGET(gtk_builder_get_object (calcBuilder, "button4"));
+  calculator->button5 = GTK_WIDGET(gtk_builder_get_object (calcBuilder, "button5"));
+  calculator->button6 = GTK_WIDGET(gtk_builder_get_object (calcBuilder, "button6"));
+  calculator->button7 = GTK_WIDGET(gtk_builder_get_object (calcBuilder, "button7"));
+  calculator->button8 = GTK_WIDGET(gtk_builder_get_object (calcBuilder, "button8"));
+  calculator->button9 = GTK_WIDGET(gtk_builder_get_object (calcBuilder, "button9"));
+  
+  calculator->buttonDot = GTK_WIDGET(gtk_builder_get_object (calcBuilder, "buttonDot"));
+  calculator->buttonEqual = GTK_WIDGET(gtk_builder_get_object (calcBuilder, "buttonEqual"));
+  calculator->buttonPlus = GTK_WIDGET(gtk_builder_get_object (calcBuilder, "buttonPlus"));
+  calculator->buttonMinus = GTK_WIDGET(gtk_builder_get_object (calcBuilder, "buttonMinus"));
+  calculator->buttonMultiply = GTK_WIDGET(gtk_builder_get_object (calcBuilder, "buttonMultiply"));
+  calculator->buttonDivide = GTK_WIDGET(gtk_builder_get_object (calcBuilder, "buttonDivide"));
+  calculator->buttonPower = GTK_WIDGET(gtk_builder_get_object (calcBuilder, "buttonPower"));
+  calculator->buttonFactorial = GTK_WIDGET(gtk_builder_get_object (calcBuilder, "buttonFactorial"));
+  calculator->buttonCE = GTK_WIDGET(gtk_builder_get_object (calcBuilder, "buttonCE"));
+  calculator->buttonC = GTK_WIDGET(gtk_builder_get_object (calcBuilder, "buttonC"));
   
   //connects signals
   gtk_builder_connect_signals (calcBuilder, calculator);
